c7b4033c34f732e8937c310b902da227581a485a
Kush Sethi <kushagra.sethi.io@gmail.com>
2024-10-12 07:05:54 -0700

    Added AkAudio to PublicDependencyModuleNames in Team_13_Game.Build.cs

--- Source/Team13_Game/Team13_Game.Build.cs
+++ Source/Team13_Game/Team13_Game.Build.cs
@@ -8,7 +8,7 @@ public class Team13_Game : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AkAudio" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
