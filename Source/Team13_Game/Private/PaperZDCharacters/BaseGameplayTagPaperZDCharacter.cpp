1f541e83ad7546dab30cd19fafcdfebb76f1b5e0
Kush Sethi <kushagra.sethi.io@gmail.com>
2024-10-25 00:05:03 -0700

    Created BaseGameplayTagPaperZDCharacter
    A C++ PaperZDCharacter that implements the IGameplayTagAssetInterface that is only available in C++.
    To be used as a way to allow Blueprint PaperZDCharacters to implement the IGameplayTagAssetInterface by re-parenting to this actor.

---
+++ Source/Team13_Game/Private/PaperZDCharacters/BaseGameplayTagPaperZDCharacter.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PaperZDCharacters/BaseGameplayTagPaperZDCharacter.h"
+
+// Sets default values
+ABaseGameplayTagPaperZDCharacter::ABaseGameplayTagPaperZDCharacter()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABaseGameplayTagPaperZDCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseGameplayTagPaperZDCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
