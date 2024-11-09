b11e362f083e635da81c55c89110242eee216467
Kush Sethi <kushagra.sethi.io@gmail.com>
2024-11-09 15:49:38 -0800

    Direction Helper Additions
    New Directions Vector containing all 8 directions in a vector.
    New TwoDVecToDirection map containing a map of 2d normalized vectors to directions.
    FacingDirectionToIndex function
    GetTwoDVecToDirection -> getter for static TwoDVecToDirection

--- Source/Team13_Game/Private/Helper/DirectionHelper.cpp
+++ Source/Team13_Game/Private/Helper/DirectionHelper.cpp
@@ -3,27 +3,55 @@
 
 #include "Helper/DirectionHelper.h"
 
-bool UDirectionHelper::IsFacingToSide(float ax, float ay)
+TMap<FVector2D, EDirection> UDirectionHelper::TwoDVecToDirection = {
+	{{ 0.0, -1.0}, EDirection::North},
+	{{ 1.0/sqrt(2), -1.0/sqrt(2)}, EDirection::NorthEast},
+	{{ 1.0,  0.0}, EDirection::East},
+	{{ 1.0/sqrt(2),  1.0/sqrt(2)}, EDirection::SouthEast},
+	{{ 0.0,  1.0}, EDirection::South},
+	{{-1.0/sqrt(2),  1.0/sqrt(2)}, EDirection::SouthWest},
+	{{-1.0,  0.0}, EDirection::West},
+	{{-1.0/sqrt(2), -1.0/sqrt(2)}, EDirection::NorthWest},
+};
+
+TArray<EDirection> UDirectionHelper::Directions = {
+	EDirection::East,
+	EDirection::SouthEast,
+	EDirection::South,
+	EDirection::SouthWest,
+	EDirection::West,
+	EDirection::NorthWest,
+	EDirection::North,
+	EDirection::NorthEast,
+};
+
+uint8 UDirectionHelper::FacingDirectionToIndex(const ABaseGamePaperZDCharacter* Character)
 {
-	return ax != 0 && ay == 0;
+	if (Character)
+	{
+		switch (Character->FacingDirection)
+		{
+			case EDirection::East:
+			case EDirection::West:
+				return 0;
+			case EDirection::South:
+				return 1;
+			case EDirection::North:
+				return 2;
+			case EDirection::SouthEast:
+			case EDirection::SouthWest:
+				return 3;
+			case EDirection::NorthWest:
+			case EDirection::NorthEast:
+				return 4;
+			default:
+				return -1;
+		}
+	}
+	return -1;
 }
 
-bool UDirectionHelper::IsFacingFront(float ax, float ay)
+TMap<FVector2D, EDirection> UDirectionHelper::GetTwoDVecToDirection()
 {
-	return ax == 0 && ay > 0;
-}
-
-bool UDirectionHelper::IsFacingBack(float ax, float ay)
-{
-	return ax == 0 && ay < 0;
-}
-
-bool UDirectionHelper::IsFacingToBackAndSide(float ax, float ay)
-{
-	return (ax > 0 && ay < 0) || (ax < 0 && ay < 0);
-}
-
-bool UDirectionHelper::IsFacingToFrontAndSide(float ax, float ay)
-{
-	return (ax > 0 && ay > 0) || (ax < 0 && ay > 0);
+	return TwoDVecToDirection;
 }
--- Source/Team13_Game/Public/Helper/DirectionHelper.h
+++ Source/Team13_Game/Public/Helper/DirectionHelper.h
@@ -3,28 +3,37 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Actors/Characters/BaseGamePaperZDCharacter.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "DirectionHelper.generated.h"
 
 /**
- * This is a Helper Blueprint Library for direction related calculations.
+ * Static Helper Functions For Direction Calculations
  */
 UCLASS()
 class TEAM13_GAME_API UDirectionHelper : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	UFUNCTION(BlueprintCallable, Category = "Helper")
-	static bool IsFacingToSide(float ax, float ay);
 
-	UFUNCTION(BlueprintCallable, Category = "Helper")
-	static bool IsFacingFront(float ax, float ay);
+public:
+	/**
+	* 8-directions in an array.
+	*
+	* Starts with East to allow bitwise optimizations during direction checking.
+	* Breaks convention used in EDirection where we start with NorthWest, but worth it for the bitwise optimization.
+	*/
+	static TArray<EDirection> Directions;
 
-	UFUNCTION(BlueprintCallable, Category = "Helper")
-	static bool IsFacingBack(float ax, float ay);
+	/**
+	* 2D Vectors To Logical Direction Mappings
+	*/
+	static TMap<FVector2D, EDirection> TwoDVecToDirection;
 
-	UFUNCTION(BlueprintCallable, Category = "Helper")
-	static bool IsFacingToBackAndSide(float ax, float ay);
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "DirectionHelper", meta = 
+		(Tooltip = "Converts the facing direction to a corresponding five direction index. 0 -> Facing East or West, 1 -> Facing South, 2 -> Facing North, 3 -> Facing SouthEast or SouthWest, 4 -> Facing NorthEast or NorthWest"))
+	static uint8 FacingDirectionToIndex(const ABaseGamePaperZDCharacter* Character);
 
-	UFUNCTION(BlueprintCallable, Category = "Helper")
-	static bool IsFacingToFrontAndSide(float ax, float ay);
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "DirectionHelper", meta =
+		(Tooltip = "Returns a map of 2D normal vectors in the XY plane to their corresponding direction."))
+	static TMap<FVector2D, EDirection> GetTwoDVecToDirection();
 };
