164a5d3da03cefbe5d615a52ab39a486fd190919
Kush Sethi <kushagra.sethi.io@gmail.com>
2024-11-09 15:24:13 -0800

    Moved Note Pool Component .cpp

--- Source/Team13_Game/Private/Components/DEPRECATED_UNotePoolComponent.cpp
+++ Source/Team13_Game/Private/Components/DEPRECATED_UNotePoolComponent.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "DEPRECATED_UNotePoolComponent.h"
+#include "Components/DEPRECATED_UNotePoolComponent.h"
+
 #include "BlueprintFunctionLibraries/DebugUtilityLibrary.h"
 
 // Sets default values for this component's properties
