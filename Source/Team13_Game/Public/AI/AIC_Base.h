2704c54d6f8383dd8ce5d3cb694f02468bd5acf5
Kush Sethi <kushagra.sethi.io@gmail.com>
2024-11-09 15:56:15 -0800

    Added AIC_Base: Base C++ AI Controller For Game

---
+++ Source/Team13_Game/Public/AI/AIC_Base.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIC_Base.generated.h"
+
+/**
+ * Base AI Controller For The Game.
+ */
+UCLASS()
+class TEAM13_GAME_API AAIC_Base : public AAIController
+{
+	GENERATED_BODY()
+
+
+	
+};
