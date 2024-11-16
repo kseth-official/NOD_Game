daf2bc3084601a624503c73436ea760327289482
Kush Sethi <kushagra.sethi.io@gmail.com>
2024-11-16 11:06:25 -0800

    Added New Functions To Direction Helper
    RotateClockwise
    RotateAnticlockwise
    FacingDirectionToIndex
    FacindgDirectionOfCharacterToIndex

--- Source/Team13_Game/Public/Helper/DirectionHelper.h
+++ Source/Team13_Game/Public/Helper/DirectionHelper.h
@@ -16,11 +16,17 @@ class TEAM13_GAME_API UDirectionHelper : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
+
+	/**
+	 * The number of directions that the player can move in.
+	 * Must be a power of 2 for bitwise optimization.
+	 */
+	static constexpr uint8 NumberOfDirections = 8;
+
 	/**
 	* 8-directions in an array.
 	*
-	* Starts with East to allow bitwise optimizations during direction checking.
-	* Breaks convention used in EDirection where we start with NorthWest, but worth it for the bitwise optimization.
+	* Starts with East to allow bitwise optimizations during direction checking following EDirection Convention.
 	*/
 	static TArray<EDirection> Directions;
 
@@ -30,10 +36,22 @@ public:
 	static TMap<FVector2D, EDirection> TwoDVecToDirection;
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "DirectionHelper", meta = 
+		(Tooltip = "Converts the facing direction of the character to a corresponding five direction index. 0 -> Facing East or West, 1 -> Facing South, 2 -> Facing North, 3 -> Facing SouthEast or SouthWest, 4 -> Facing NorthEast or NorthWest"))
+	static uint8 FacingDirectionOfCharacterToIndex(const ABaseGamePaperZDCharacter* Character);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "DirectionHelper", meta =
 		(Tooltip = "Converts the facing direction to a corresponding five direction index. 0 -> Facing East or West, 1 -> Facing South, 2 -> Facing North, 3 -> Facing SouthEast or SouthWest, 4 -> Facing NorthEast or NorthWest"))
-	static uint8 FacingDirectionToIndex(const ABaseGamePaperZDCharacter* Character);
+	static uint8 FacingDirectionToIndex(const EDirection& Direction);
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "DirectionHelper", meta =
 		(Tooltip = "Returns a map of 2D normal vectors in the XY plane to their corresponding direction."))
 	static TMap<FVector2D, EDirection> GetTwoDVecToDirection();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "DirectionHelper", meta =
+		(Tooltip = "Rotates the provided direction clockwise by angle."))
+	static EDirection RotateClockwise(const EDirection& Direction, const float& Angle);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "DirectionHelper", meta =
+		(Tooltip = "Rotates the provided direction anticlockwise by angle."))
+	static EDirection RotateAntiClockwise(const EDirection& Direction, const float& Angle);
 };
